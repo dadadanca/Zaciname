4c0fd90d31a3ba1d6826127c98c6f72ff2a932fb
dadadanca <dada.013@seznam.cz>
2017-05-02 15:51:56 +0200

    Pismena zvetsena

--- Kapitalky.py
+++ Kapitalky.py
@@ -1,8 +1,16 @@
 veta = "Toto je veta, ktera by cela mely byt napsana slovy s velkymi pocatecnimi pismeny."
 
 slova = veta.split(" ")
-slova = map(lambda slovo: slovo.upper(), slova)
+
+def ZvetsiSlovo (slovo):
+    if len(slovo)>1:
+        return slovo[0].upper()+ slovo[1::]
+    else:
+        return slovo
+
+slova = map(ZvetsiSlovo, slova)
 velkaSlova = list(slova)
 print(velkaSlova)
 
+print(ZvetsiSlovo("josef"))
 print(" ".join(velkaSlova))
