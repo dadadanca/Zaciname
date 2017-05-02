de61f6568c4d5af776bf0f2bcc82bb730e321aa7
Vaclav <vaclav.pech@gmail.com>
2017-05-02 15:54:13 +0200

    Hahaha

--- Kapitalky.py
+++ Kapitalky.py
@@ -1,7 +1,14 @@
 veta = "Toto je veta, ktera by cela mely byt napsana slovy s velkymi pocatecnimi pismeny."
 
 slova = veta.split(" ")
-slova = map(lambda slovo: slovo.upper(), slova)
+
+def zvetsiSlovo(slovo):
+    if len(slovo) > 1:
+        return slovo[0].upper() + slovo[1::]
+    else:
+        return slovo
+
+slova = map(zvetsiSlovo, slova)
 velkaSlova = list(slova)
 print(velkaSlova)
 
