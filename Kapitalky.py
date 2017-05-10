2aeeba7029b5a8801fc10cc6b61c534641d2aeea
dadadanca <dada.013@seznam.cz>
2017-05-10 15:43:54 +0200

    Kapitalky zvetseni textu s uzivatelem

--- Kapitalky.py
+++ Kapitalky.py
@@ -1,17 +1,18 @@
-veta = "Toto je veta, ktera by cela mely byt napsana slovy s velkymi pocatecnimi pismeny."
+veta = input("Zadejte vetu ke zvetseni:")
+while veta!="":
+    slova = veta.split(" ")
 
-slova = veta.split(" ")
+    def ZvetsiSlovo (slovo):
+        if len(slovo)>1:
+            return slovo[0].upper()+ slovo[1::]
+        else:
+            return slovo
+    # Dana: map aplikuje funkci ZvetsiSlovo na všechna slova
+    slova = map(ZvetsiSlovo, slova)
+    velkaSlova = list(slova)
 
-def ZvetsiSlovo (slovo):
-    if len(slovo)>1:
-        return slovo[0].upper()+ slovo[1::]
-    else:
-        return slovo
-# Dana: map aplikuje funkci ZvetsiSlovo na všechna slova
-slova = map(ZvetsiSlovo, slova)
-velkaSlova = list(slova)
-print(velkaSlova)
-
-print(" ".join(velkaSlova))
-# vytvori mi vetu z velkaSlova
+    print(" ".join(velkaSlova))
+    # vytvori mi vetu z velkaSlova
+    veta = input("Zadejte dalsi vetu ke zvetseni:")
+print("Diky, ahoj")
 
