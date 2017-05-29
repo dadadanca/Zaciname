86391495c8410cae2bd7dde187d0b733a4b76462
dadadanca <dada.013@seznam.cz>
2017-05-29 13:59:17 +0200

    Dotaznik

---
+++ Dotaznik.py
@@ -0,0 +1,24 @@
+zdrojovyDokument = open("Answers.csv")
+acc = 0
+v = 0
+prvniRadek = True
+jmenoNejstarsiho = ""
+
+for radek in zdrojovyDokument:
+    if prvniRadek:
+        prvniRadek = False
+        continue
+    jedenFormular = radek.strip("\n").split(",")
+    if jedenFormular[2] == "Yes":
+        acc = acc + 1 + int(jedenFormular[4])
+        if int(jedenFormular[3]) > v:
+            v = int(jedenFormular[3])
+            jmenoNejstarsiho = jedenFormular[1]
+
+
+print(acc)
+print(v)
+print(jmenoNejstarsiho)
+
+zdrojovyDokument.close()
+# parcujeme dokument
