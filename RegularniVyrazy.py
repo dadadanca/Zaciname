13fbdbb44ad036b51f3451c407f46777bb60220d
dadadanca <dada.013@seznam.cz>
2017-06-08 13:55:50 +0200

    Groupy a Matche

--- RegularniVyrazy.py
+++ RegularniVyrazy.py
@@ -30,7 +30,42 @@ print(re.findall(r"[D|d]an\d*", "Dan guwgdid Dan2 agshjdgwj Dan3 QHHDWQHD Dan4."
 print(re.findall(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9-.]+[.][a-zA-Z]{2,3}$", "honza@seznam.cz"))
 
 
+print(re.split(r"\W", "Ahoj Vasku, jak se mas? Uz jdes domu? Ja jeste ne."))
+print(re.split(r"[^a-zA-Z0-9_]", "Ahoj Vasku, jak se mas? Uz jdes domu? Ja jeste ne."))
+# rozdeli podle vseho co muze byt soucasti slova
 
+match = re.search(r"[0-9]+", "Ahoj Vasku, jak 5 se mas? Uz jdes 12 domu? Ja jeste3 ne.")
+print(match.start())
+print(match.end())
+print(match.group())
+# rekne mi co je ten match, ten prvni
 
+matches = re.finditer(r"[0-9]+", "Ahoj Vasku, jak 5 se mas? Uz jdes 12 domu? Ja jeste3 ne.")
+# findeiter najde vsechny matche a group mi je vypise
+for match in matches:
+    print(match.group())
 
+match = re.match(r"[^0-9]+", "Ahoj Vasku, jak 5 se mas? Uz jdes 12 domu? Ja jeste3 ne.")
+print(match.group())
+# najde vsechno co neobsahuje cisla - nejdelsi retezec pred cislem
 
+match = re.fullmatch(r"[a-zA-Z0-9,\? \.]+", "Ahoj Vasku, jak 5 se mas? Uz jdes 12 domu? Ja jeste3 ne.")
+print(match.group())
+#overi, zda cely vyraz odpovida memu zadani
+
+match = re.search(r"([0-9]+) ?([*/+-]) ?([0-9]+)", "Ahoj ahoj 6 * 5ahoj ahoooj")
+print(match.group())
+print(match.group(1))
+print(match.group(2))
+print(match.group(3))
+
+a=int(match.group(1))
+b=int(match.group(3))
+if match.group(2) == "+":
+    print(a+b)
+if match.group(2) == "-":
+    print(a-b)
+if match.group(2) == "*":
+    print(a*b)
+if match.group(2) == "/":
+    print(a/b)
