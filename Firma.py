a44aad5480ab182e32a7e936139256419952aa83
dadadanca <dada.013@seznam.cz>
2017-07-18 08:46:00 +0200

    ObjektoveProgramovani - JetBrains najima zamestnance

--- Firma.py
+++ Firma.py
@@ -4,16 +4,31 @@ class Firma:
         self.zamestnanci = []
         self.reditel = None
 
+    def LzePrijmout(self,k):
+        for z in self.zamestnanci:
+            if z.role==k.role:
+                return False
+        return True
+
     def najmi(self, z):
         self.zamestnanci.append(z)
 
     def propust(self, z):
         self.zamestnanci.remove(z)
+        if self.reditel==z:
+            self.reditel=None
+
+    def vypisZamestnance(self):
+        for z in self.zamestnanci:
+            print(z)
 
 class Zamestnanec:
     def __init__(self, jmeno, role):
         self.jmeno = jmeno
         self.role = role[0].upper() + role[1::]
+
+    def __str__(self):
+        return "%s - %s"%(self.jmeno,self.role)
 
 jb = Firma("JetBrains")
 e = Zamestnanec("Eda", "skladnik")
@@ -22,5 +37,10 @@ jb.najmi(Zamestnanec("Alice", "programatorka"))
 jb.najmi(Zamestnanec("David", "kuchar"))
 
 jb.reditel = e
+jb.vypisZamestnance()
+print(jb.LzePrijmout(Zamestnanec("Paja", "pravnicka")))
+print(jb.LzePrijmout(Zamestnanec("Paja", "programatorka")))
+jb.propust(e)
+
 
 
