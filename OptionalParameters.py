af9350db3478d336424662def3f51771bd3983e5
dadadanca <dada.013@seznam.cz>
2017-10-16 15:01:54 +0200

    Optional Parameters 4.kap

---
+++ OptionalParameters.py
@@ -0,0 +1,10 @@
+
+
+def vypis_jmena(jmena, oddelovac=", "):
+    print(oddelovac.join(jmena))
+
+jmena=["Franta", "Lucie", "Adam", "Marcela"]
+vypis_jmena(jmena, ": ")
+vypis_jmena(jmena)
+print(x)
+
