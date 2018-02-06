c7ce2356c23529d6d3afe2ef77f8274f27ec41b6
dadadanca <dada.013@seznam.cz>
2018-02-06 16:12:25 +0100

    QuickSort

--- Tridime.py
+++ Tridime.py
@@ -32,3 +32,45 @@ while bylaZmena:
         print(cisla)
 
 print(cisla)
+
+# quick sort
+cisla = [12, 3, 87, 46, 67, 15, 9, 2, 32, 99, 1, 56, 42, 38, 27, 18]
+def quick_sort (seznam):
+    print(seznam)
+    if len(seznam)<=1:
+        return seznam
+    pozice_pivota = 0
+    levy_index = 1
+    pravy_index = len(seznam)-1
+    while levy_index<=pravy_index:
+        if pozice_pivota < levy_index:
+            if seznam[pozice_pivota] > seznam[pravy_index]:
+                kapsa=seznam[pozice_pivota]
+                seznam[pozice_pivota]=seznam[pravy_index]
+                seznam[pravy_index]= kapsa
+                pozice_pivota = pravy_index
+            pravy_index-=1
+        else:
+            if seznam[pozice_pivota] < seznam[levy_index]:
+                kapsa=seznam[pozice_pivota]
+                seznam[pozice_pivota]=seznam[levy_index]
+                seznam[levy_index]= kapsa
+                pozice_pivota = levy_index
+            levy_index+=1
+    prvni_cast = quick_sort(seznam[0:pozice_pivota])
+    druha_cast = quick_sort(seznam[pozice_pivota+1::])
+
+    return prvni_cast + [seznam[pozice_pivota]] + druha_cast
+
+
+print(quick_sort(cisla))
+
+
+
+
+
+
+
+
+
+
